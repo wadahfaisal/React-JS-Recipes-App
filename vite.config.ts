e77790f732f179d1a06468b79c29c8f41c67c681
unknown <wadah.faisal99@gmail.com>
2024-03-04 23:29:54 +0200

    changing url

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "https://www.themealdb.com/api/json/v1/1",
-        target: "https://api.spoonacular.com",
+        // target: "https://api.spoonacular.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
