421449182c8841e1a94616939dcbbb23463c327c
unknown <wadah.faisal99@gmail.com>
2024-03-04 22:43:10 +0200

    installing dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-JS-Recipes-App/",
   plugins: [react()],
+
   server: {
     proxy: {
       "/api": {
         // target: "https://www.themealdb.com/api/json/v1/1",
-        target: " https://api.spoonacular.com",
+        target: "https://api.spoonacular.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
